5bb3a1e32d9e531391197747bf741af26e378109
belisa bettega <belisabettega@gmail.com>
2021-09-07 14:21:15 -0300

    feat: update, create and delete endpoints

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -1,11 +1,36 @@
 class Api::V1::RestaurantsController < Api::V1::BaseController
-  before_action :set_restaurant, only: [:show]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_restaurant, only: [:show, :update, :destroy]
 
   def show
   end
 
+  def update
+    if @restaurant.update(restaurant_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
+    if @restaurant.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   def index
     @restaurants = policy_scope(Restaurant)
+  end
+
+  def destroy
+    @restaurant.destroy
+    head :no_content
   end
 
   private
@@ -13,5 +38,14 @@ class Api::V1::RestaurantsController < Api::V1::BaseController
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
     authorize @restaurant
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address)
+  end
+
+  def render_error
+    render json: { errors: @restaurant.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
